80a6ae0a6293867ce636fce45a1e3fb4fd812f2e
unknown <jkumari1990@gmail.com>
2026-02-09 23:38:01 +0530

    add /admin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import api from "../config/axios";
 // ✅ ENV BASE URL
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  
 
 export default function Header() {
   const navigate = useNavigate();
